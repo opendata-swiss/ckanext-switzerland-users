6d8ef15a66864d7fba9facd5a448ef6f5d7e40bd
sabinem <sabine.maennel@gmail.com>
2021-03-11 09:23:45 +0100

    adapt test.ini and build script to the used ckan extensions

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -63,11 +63,6 @@ cd ckanext-switzerland_users
 python setup.py develop
 cd -
 
-echo "Installing ckanext-switzerland and its requirements..."
-python setup.py develop
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
-
 echo "Moving test.ini into a subdir..."
 mkdir subdir
 mv test.ini subdir
